c1bcec6464fa4476644dee9cf1a56d684b6fd58d
freitzzz <1160907@isep.ipp.pt>
2021-03-19 23:49:42 +0000

    Change divider color

--- lib/src/list_tile.dart
+++ lib/src/list_tile.dart
@@ -45,6 +45,8 @@ class CupertinoListTile extends StatelessWidget {
     this.focusColor,
     this.hoverColor,
     this.focusNode,
+    this.dividerColor,
+    this.dividerThickness,
     this.autofocus = false,
   })  : assert(isThreeLine != null),
         assert(enabled != null),
@@ -107,11 +109,17 @@ class CupertinoListTile extends StatelessWidget {
   /// See [ListTile.autofocus].
   final bool autofocus;
 
+  final Color dividerColor;
+
+  final double dividerThickness;
+
   Color _iconColor(
       BuildContext context, CupertinoThemeData theme, ListTileTheme tileTheme) {
     if (!enabled)
       return CupertinoDynamicColor.resolve(
-          CupertinoColors.placeholderText, context);
+        CupertinoColors.placeholderText,
+        context,
+      );
 
     if (selected && tileTheme?.selectedColor != null)
       return tileTheme.selectedColor;
@@ -121,22 +129,6 @@ class CupertinoListTile extends StatelessWidget {
     if (selected) return theme.primaryColor;
 
     return null;
-  }
-
-  Color _textColor(BuildContext context, CupertinoThemeData theme,
-      ListTileTheme tileTheme, Color defaultColor) {
-    if (!enabled)
-      return CupertinoDynamicColor.resolve(
-          CupertinoColors.placeholderText, context);
-
-    if (selected && tileTheme?.selectedColor != null)
-      return tileTheme.selectedColor;
-
-    if (!selected && tileTheme?.textColor != null) return tileTheme.textColor;
-
-    if (selected) return theme.primaryColor;
-
-    return defaultColor;
   }
 
   bool _isDenseLayout(ListTileTheme tileTheme) {
@@ -150,8 +142,9 @@ class CupertinoListTile extends StatelessWidget {
 
     IconThemeData iconThemeData;
     if (leading != null || trailing != null)
-      iconThemeData =
-          IconThemeData(color: _iconColor(context, theme, tileTheme));
+      iconThemeData = IconThemeData(
+        color: _iconColor(context, theme, tileTheme),
+      );
 
     Widget leadingIcon;
     if (leading != null) {
@@ -205,10 +198,12 @@ class CupertinoListTile extends StatelessWidget {
         decoration: BoxDecoration(
           border: Border(
             bottom: BorderSide(
-              color: CupertinoDynamicColor.resolve(
-                CupertinoColors.separator,
-                context,
-              ),
+              color: dividerColor ??
+                  CupertinoDynamicColor.resolve(
+                    CupertinoColors.separator,
+                    context,
+                  ),
+              width: dividerThickness ?? 1,
             ),
           ),
         ),
